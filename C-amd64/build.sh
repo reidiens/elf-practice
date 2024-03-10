f8d9246d0328f1106391a2eb989340f02c25f3ae
Ruben Toledo <tibamita@tutamail.com>
2024-03-10 17:20:46 -0400

    fixed elf files not being executable

--- C-amd64/build.sh
+++ C-amd64/build.sh
@@ -11,6 +11,7 @@ chmod +x ${PNAME}
 if [ "$1" != "" ]; then
     echo "generating elf file..."
     ./${PNAME} "$1"
+    chmod +x ./"$1"
 
 else
     echo ""
@@ -23,10 +24,12 @@ else
         sleep 1
         echo "generating elf file..."
         ./${PNAME} ${DEFAULT}
+        chmod +x ${DEFAULT}
     
     else 
         echo "generating elf file..."
         ./${PNAME} ${ELFNAME}
+        chmod +x ./${ELFNAME}
 
     fi
 fi
--- C-i386/build.sh
+++ C-i386/build.sh
@@ -6,11 +6,11 @@ DEFAULT=i386-elf-file
 echo "building..."
 make PROG=${PNAME}
 echo "built!"
-chmod +x ${PNAME}
 
 if [ "$1"= "" ]; then
     echo "generating elf file..."
     ./${PNAME} "$1"
+    chmod +x ./"$1" 
 
 else 
     echo ""
@@ -23,10 +23,12 @@ else
         sleep 1
         echo "generating elf file..."
         ./${PNAME} ${DEFAULT}
+        chmod +x ./${DEFAULT}
 
     else 
         echo "generating elf file..."
         ./${PNAME} ${ELFNAME}
+        chmod +x ./${ELFNAME}
     
     fi
 fi
