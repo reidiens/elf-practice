9e76f913b602bd2b2e7c0f8619bb99eb060f94cc
Ruben Toledo <tibamita@tutamail.com>
2024-03-12 00:09:48 -0400

    fuck

--- ASM-i386/hello-elf-i386.s
+++ ASM-i386/hello-elf-i386.s
@@ -1,26 +1,29 @@
 global _start
 
 section .data
-    fname   db  "file",0
+    fname   db  "file",0                ; temp var
 
-    msg1    db "creating file...",0xA
+    msg1    db "creating file...",0xA   
     msg1len equ $ - msg1
 
     msg2    db "file created!", 0xA
     msg2len equ $ - msg2
 
-    err     db "file not opened.",0xA
+    fmsg    db "still figuring it out...",0xA
+    fmsglen equ $ - fmsg
+
+    err     db "file not created.",0xA
     errlen  equ $ - err
 
     EXIT    equ 0x01
     WRITE   equ 0x04
+    CREAT   equ 0x08
     OPEN    equ 0x05
+    CLOSE   equ 0x06
 
     STDOUT  equ 0x01
 
-    WRONLY  equ 1o
-    CREAT   equ 100o
-    TRUNC   equ 1000o
+    perms   equ 0333o
 
 section .text
 
@@ -31,34 +34,21 @@ _start:
     mov     edx, msg1len
     int 80h
 
-    mov     eax, OPEN
+    mov     eax, CREAT
     mov     ebx, fname
-    mov     ecx, CREAT+WRONLY
-    or      ecx, CREAT
+    mov     ecx, perms
     int 80h
-
-    mov     esi, eax
-    cmp     esi, -1
-    je      error
 
     mov     eax, WRITE
-    mov     ebx, STDOUT
-    mov     ecx, msg2
-    mov     edx, msg2len
-    int 80h
+    mov     ebx, esi
+    mov     ecx, fmsg
+    mov     edx, fmsglen
+    int 80h 
 
-ret:
+    mov     eax, CLOSE
+    mov     ebx, esi
+    int 80h 
+
     mov     eax, 0x01
     mov     ebx, 0x00
     int 80h
-
-error:
-    mov     eax, WRITE
-    mov     ebx, STDOUT
-    mov     ecx, err
-    mov     edx, errlen
-    int 80h
-
-    goto ret
-
-
